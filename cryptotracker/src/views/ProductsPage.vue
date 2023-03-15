0708929f5aa7036b25a61d2220444077e9d1f12a
AlenOrbanic <aorbanic@student.unipu.hr>
2023-03-15 03:49:25 +0100

    Razdvajanje koda na dijelove, korištenje propova

--- cryptotracker/src/views/ProductsPage.vue
+++ cryptotracker/src/views/ProductsPage.vue
@@ -1,64 +1,23 @@
 <template>
-<div id="page-wrap">
-  <div class="grid-wrap">
-<div v-for="product in products" class="product-item" v-bind:key="product.id">
-  <img v-bind:src="product.imageUrl"/>
-<h3 clas="product-name">{{ product.name }}}</h3>   
-<p class="product-price">${{ product.price }}}</p>
-<router-link v-bind:to="'/products/'+product.id"> <!--router link je bolji od anchora jer nam ne refresha str.-->
-<button>View details</button></router-link>
-    </div>
+  <div id="page-wrap">
+    <ProductsGrid :products="products" />
   </div>
-</div>    
 </template>
 
-<style scoped>
-  .grid-wrap {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    margin-top: 16px;
-  }
-
-  .product-item {
-    align-items: center;
-    border-radius: 8px;
-    box-shadow: 0px 2px 5px #888;
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 2%;
-    padding: 20px;
-    position: relative;
-    width: 32%;
-  }
-
-  .product-name {
-    margin-bottom: 0;
-  }
-
-  img {
-    height: 200px;
-    width: 200px;
-  }
-
-  a {
-    width: 100%;
-  }
-
-  button {
-    width: 100%;
-  }
-</style>
-
 <script>
-import {products} from '../fake-data';
-export default{
+import {products} from '../fake-data'
+import ProductsGrid from '../components/ProductsGrid.vue';
+
+export default {
     name: 'ProductsPage',
-    data(){
-      return{
-        title: 'Hello!',
-        products,
+    components: {
+      ProductsGrid,
+    },
+    data() {
+      return {
+        products
       };
-    }
+    },
 };
 </script>
+
